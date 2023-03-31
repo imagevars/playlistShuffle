924a6ef8fe61f92296f8f58076cde6e277217837
jonathan <jonathan>
2023-03-31 09:04:48 -0300

    html title change

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Routes>
         <Route
           exact
-          path="/playlistShuffle/"
+          path="playlistShuffle/"
           element={
             <>
               <Home />
