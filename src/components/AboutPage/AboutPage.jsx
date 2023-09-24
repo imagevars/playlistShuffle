736cc189d8bebddbae1b0e905f91d8382b43b472
jooonathann <jooni.escobar.98@gmail.com>
2023-09-23 21:47:56 -0300

    about page fixed

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -8,11 +8,11 @@ function AboutPage() {
   return (
     <div className="flex flex-col justify-between transition-colors bg-backColor image:bg-[unset]  h-screen min-h-screen">
       <Navbar />
-      <div className="flex text-primaryColor dark:text-DarkPrimaryColor justify-center items-center bg-bgWhite dark:bg-bgBlack">
-        <div className="text-bgBlack dark:text-bgWhite bg-bgWhite dark:bg-bgBlack  text-base w-4/4 md:w-3/5 text-justify">
+      <div className="flex justify-center items-center">
+        <div className="text-textColor text-base w-4/4 md:w-3/5 text-justify">
           <p className="font-open mx-4 my-2 list-disc">
             If you like the page don&apos;t forget the give a star ★ to the github
-            <a className="text-primaryColor dark:text-DarkPrimaryColor font-semibold font-open hover:scale-110" href="https://github.com/jooonathann/playlistShuffle" target="_blank" rel="noopener noreferrer" aria-label="github link"> repo</a>
+            <a className="text-primary  font-semibold font-open hover:scale-110" href="https://github.com/jooonathann/playlistShuffle" target="_blank" rel="noopener noreferrer" aria-label="github link"> repo</a>
             , its free (for now).
           </p>
           <p className="font-open mx-4 my-2">I made this because the shuffle algo of Youtube does not shuffle, and the other pages that I&apos;ve tried didn&apos;t had the things that I wanted.</p>
