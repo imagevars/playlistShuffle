e4ba460818dba2033c8e41d0bb50b40658de7d08
jooonathann <jooni.escobar.98@gmail.com>
2023-09-26 03:40:16 -0300

    github icon on navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,9 @@
-import React, { memo, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { connect } from "react-redux";
-import {
-  BsFillMoonFill,
-  BsFillSunFill,
-  BsTwitter,
-  BsImageFill,
-} from "react-icons/bs";
-import PropTypes from "prop-types";
+import React, { memo, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { BsFillMoonFill, BsFillSunFill, BsImageFill } from 'react-icons/bs';
+import { AiFillGithub } from 'react-icons/ai';
+import PropTypes from 'prop-types';
 import {
   isPlaying,
   currentSong,
@@ -16,7 +12,7 @@ import {
   setTheme,
   setTitle,
   setArtist,
-} from "../../redux/actions/playerActions";
+} from '../../redux/actions/playerActions';
 
 function Navbar({
   isPlaying,
@@ -32,64 +28,66 @@ function Navbar({
   const handleClickHome = () => {
     isPlaying(true);
     isShuffleActive(false);
-    setCurrentActivePlaylistId("");
-    setTitle("");
-    setArtist("");
+    setCurrentActivePlaylistId('');
+    setTitle('');
+    setArtist('');
 
-    return navigate("/");
+    return navigate('/');
   };
 
   useEffect(() => {
-    if (player.theme === "light") {
-      document.documentElement.classList.remove("dark");
-      document.documentElement.classList.remove("image");
-      document.documentElement.classList.add("light");
+    if (player.theme === 'light') {
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove('image');
+      document.documentElement.classList.add('light');
     }
-    if (player.theme === "dark") {
-      document.documentElement.classList.remove("light");
-      document.documentElement.classList.remove("image");
-      document.documentElement.classList.add("dark");
+    if (player.theme === 'dark') {
+      document.documentElement.classList.remove('light');
+      document.documentElement.classList.remove('image');
+      document.documentElement.classList.add('dark');
     }
-    if (player.theme === "image") {
-      document.documentElement.classList.remove("light");
-      document.documentElement.classList.remove("dark");
-      document.documentElement.classList.add("image");
+    if (player.theme === 'image') {
+      document.documentElement.classList.remove('light');
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('image');
     }
   }, []);
 
   const handleClickTheme = () => {
-    if (player.theme === "light") {
-      document.documentElement.classList.remove("image");
-      document.documentElement.classList.remove("light");
-      document.documentElement.classList.add("dark");
-      setTheme("dark");
+    if (player.theme === 'light') {
+      document.documentElement.classList.remove('image');
+      document.documentElement.classList.remove('light');
+      document.documentElement.classList.add('dark');
+      setTheme('dark');
     }
-    if (player.theme === "dark") {
-      document.documentElement.classList.remove("dark");
-      document.documentElement.classList.remove("light");
-      document.documentElement.classList.add("image");
-      setTheme("image");
+    if (player.theme === 'dark') {
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.remove('light');
+      document.documentElement.classList.add('image');
+      setTheme('image');
     }
-    if (player.theme === "image") {
-      document.documentElement.classList.remove("image");
-      document.documentElement.classList.remove("dark");
-      document.documentElement.classList.add("light");
-      setTheme("light");
+    if (player.theme === 'image') {
+      document.documentElement.classList.remove('image');
+      document.documentElement.classList.remove('dark');
+      document.documentElement.classList.add('light');
+      setTheme('light');
     }
   };
 
   return (
     <div className=" w-full flex justify-between px-1">
-      <div className="flex justify-between w-full mx-2 md:max-w-[2000x]">
+      <div className="flex justify-between w-full mx-4 my-1 md:max-w-[2000x]">
         {/* eslint-disable-next-line */}
         <a
-          href="https://twitter.com/Jonathhn1"
-          className="w-24"
           target="_blank"
           rel="noopener noreferrer"
           aria-label="github link"
+          href="https://github.com/jooonathann/playlistShuffle"
         >
-          <BsTwitter size={25} fill="#1DA1F2" />
+          <AiFillGithub
+            size={30}
+            className="mx-3 my-1 text-primary hover:text-accent "
+          />
         </a>
         {/* eslint-disable-next-line */}
         <h1
@@ -97,9 +95,9 @@ function Navbar({
           cursor="pointer"
           onClick={handleClickHome}
         >
-          Shuffle Playlist{" "}
+          Shuffle Playlist{' '}
         </h1>
-        {player.theme === "image" && (
+        {player.theme === 'image' && (
           <div className="w-24 flex justify-end">
             <BsFillSunFill
               fill="white"
@@ -110,7 +108,7 @@ function Navbar({
             />
           </div>
         )}
-        {player.theme === "dark" && (
+        {player.theme === 'dark' && (
           <div className="w-24 flex justify-end">
             <BsImageFill
               fill="white"
@@ -121,7 +119,7 @@ function Navbar({
             />
           </div>
         )}
-        {player.theme === "light" && (
+        {player.theme === 'light' && (
           <div className="w-24 flex justify-end">
             <BsFillMoonFill
               fill="black"
