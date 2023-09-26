c87f471ef21ae075cc06d82e2b4f62e9ce428b15
jooonathann <jooni.escobar.98@gmail.com>
2023-09-26 02:58:47 -0300

    background picture when theres no currentSong active

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,33 @@
-import React, { useEffect, useRef } from "react";
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useRef } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Route, Routes } from 'react-router-dom';
 // eslint-disable-next-line import/no-unresolved
-import { Analytics } from "@vercel/analytics/react";
-import HomePage from "./components/HomePage/HomePage";
-import PlaylistPage from "./components/PlaylistPage/PlaylistPage";
-import "./app.css";
-import ErrorPage from "./components/ErrorPage/ErrorPage";
-import AboutPage from "./components/AboutPage/AboutPage";
+import { Analytics } from '@vercel/analytics/react';
+import HomePage from './components/HomePage/HomePage';
+import PlaylistPage from './components/PlaylistPage/PlaylistPage';
+import './app.css';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import AboutPage from './components/AboutPage/AboutPage';
 
 function App({ player }) {
   const ref = useRef(null);
   useEffect(() => {
-    if (player.theme === "image") {
-      if (player.currentSong === "") {
-        ref.current.style.backgroundColor = "#404040";
-        ref.current.style.backgroundImage = "none";
-        ref.current.style.transition = "background 700ms ease-in-out 150ms";
+    if (player.theme === 'image') {
+      ref.current.style.transition = 'background 700ms ease-in-out 150ms';
+      ref.current.style.backgroundBlendMode = 'multiply';
+      ref.current.style.backgroundSize = 'cover';
+      ref.current.style.backgroundPosition = 'center';
+      if (player.currentSong === '') {
+        ref.current.style.backgroundImage =
+          'url(./assets/images/silivan-munguarakarama-NrR9gn3lFKU-unsplash.jpg)';
       } else {
-        ref.current.style.transition = "background 700ms ease-in-out 150ms";
         ref.current.style.backgroundImage = `url(https://i.ytimg.com/vi/${player.currentSong}/hqdefault.jpg)`;
-        ref.current.style.backgroundBlendMode = "multiply";
-        ref.current.style.backgroundSize = "cover";
-        ref.current.style.backgroundPosition = "center";
-        ref.current.style.backgroundColor = "#404040";
       }
+      ref.current.style.backgroundColor = '#404040';
     }
-  }, [player.currentSong]);
+  }, [player.currentSong, player.theme]);
 
-  useEffect(() => {
-    if (player.theme === "image" && player.currentSong !== "") {
-      ref.current.style.transition = "background 700ms ease-in-out 150ms";
-      ref.current.style.backgroundImage = `url(https://i.ytimg.com/vi/${player.currentSong}/hqdefault.jpg)`;
-      ref.current.style.backgroundBlendMode = "multiply";
-      ref.current.style.backgroundSize = "cover";
-      ref.current.style.backgroundPosition = "center";
-      ref.current.style.backgroundColor = "#404040";
-    } else {
-      ref.current.style.backgroundColor = "#404040";
-      ref.current.style.backgroundImage = "none";
-    }
-  }, [player.theme]);
   return (
     <div ref={ref} id="app">
       <div className="backdrop-blur-sm">
