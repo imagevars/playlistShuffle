f7af6c90200c390c666399120bee5a259b8bd5fc
Jonathan <jooni.escobar.98@gmail.com>
2024-02-29 19:47:26 -0300

    twitter and github icons deleted from footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,33 +16,11 @@ function Footer() {
   return (
     <footer className="">
       <div className="flex justify-center">
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="github link"
-          href="https://github.com/jooonathann/playlistShuffle"
-        >
-          <AiFillGithub
-            size={30}
-            className="mx-3 my-1 text-primary light:hover:text-[#333333] hover:text-textColorInside"
-          />
-        </a>
         <AiFillInfoCircle
           onClick={handleClick}
           size={30}
           className="mx-3 my-1 text-primary cursor-pointer hover:text-secondary"
         />
-        <a
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="twitter link"
-          href="https://twitter.com/Jonathhn1"
-        >
-          <AiOutlineTwitter
-            size={30}
-            className="mx-3 my-1 text-primary hover:text-[#1da1f2] dark:hover:text-[#1da1f2]"
-          />
-        </a>
       </div>
       <div className="flex items-center justify-center">
         <p className="text-textColor text-center my-1 font-open">
